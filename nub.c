24df94aca6d1cdd1a625fc76342caae81bcf9377
Nbishen20 <nageshwar.singh_cs15@gla.ac.in>
2018-01-31 13:27:57 +0530

    +ve -ve program

---
+++ nub.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main(){
+    int n;
+    scanf("%d",&n);
+    if(n>0)
+     printf("\n Positive");
+    else if(n<0)
+     printf("\n Negative");
+    else
+     printf("\n Zero");
+}
